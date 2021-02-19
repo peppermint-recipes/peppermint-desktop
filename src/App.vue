c60af1f300589799b07d770930644c011b74c27e
André Sterba <48120735+andresterba@users.noreply.github.com>
2021-02-19 22:15:17 +0000

    Init peppermint desktop (#1)
    Co-authored-by: theErikss0n <dev@der-klinger.de>
    Co-authored-by: eriksson <github@der-klinger.de>

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item
+          link
+          to="/recipe"
+        >
+          <v-list-item-action>
+            <v-icon>mdi-plus</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>New Recipe</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item
+          link
+          to="/recipes"
+        >
+          <v-list-item-action>
+            <v-icon>mdi-pot-mix</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Recipes</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item
+          link
+          to="/week"
+        >
+          <v-list-item-action>
+            <v-icon>mdi-calendar</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Week</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item
+          link
+          to="/shopping-list"
+        >
+          <v-list-item-action>
+            <v-icon>mdi-cart</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Shopping List</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+    drawer: true,
+  }),
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify',
+  ],
+  configureWebpack: {
+    resolve: {
+      symlinks: true,
+    },
+  },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        // options placed here will be merged with default configuration and passed to electron-builder
+        appId: "peppermint-recipes",
+        linux: {
+          category: "Utility",
+          desktop: "peppermint-recipes",
+        },
+      },
+    },
+  },
+  configureWebpack: {
+    resolve: { symlinks: false }
+  }
+};
